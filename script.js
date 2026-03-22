280b8b57a4545d5f6b1f2a5d721f28eabb69b628
Lana Karine <karinelana03@gmail.com>
2026-03-22 01:53:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// botão topo
-const btn = document.getElementById("btnTopo");
+const elementos = document.querySelectorAll(".card");
 
-btn.addEventListener("click", () => {
+window.addEventListener("scroll", () => {
+    elementos.forEach(el => {
+        const posicao = el.getBoundingClientRect().top;
+
+        if (posicao < window.innerHeight - 50) {
+            el.classList.add("show");
+        }
+    });
+});
+
+const btnTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        btnTopo.style.display = "block";
+    } else {
+        btnTopo.style.display = "none";
+    }
+});
+
+btnTopo.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
 });
-
-// animação ao aparecer
-const elementos = document.querySelectorAll(".hidden");
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-    });
-});
-
-elementos.forEach(el => observer.observe(el));
